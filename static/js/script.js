d12f84fe818adaebb04b32054efabd68e1fdf161
Twene Twene Appiah <159663841+Twenethomas@users.noreply.github.com>
2025-03-16 11:44:48 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // Establish Socket.IO connection
-const socket = io.connect("https://tomchatapp.onrender.com");
+const socket = io.connect("https://192.168.1.10:5000");
 
 // ----- Sidebar & Tab Functions -----
 function openTab(tabName) {
