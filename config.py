36556e7c9d795b21c2fda9adfb9d55bdb2acb1a6
Twene Twene Appiah <twenethomas36@gmail.com>
2025-03-16 06:25:17 +0000

    added the database url for postgresql

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
     
     # ✅ Securely load database URL from environment variables
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://thomas:naehW2yqrF4A4WVScbyCkZK8k62n9vOB@dpg-cvb6gjlumphs73alc800-a/tomchat_db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
