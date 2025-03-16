6b739c49d3a58708ca6c672d769d20c7f88769ee
Twene Twene Appiah <159663841+Twenethomas@users.noreply.github.com>
2025-03-16 09:30:31 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+    SECRET_KEY = os.getenv("SECRET_KEY", "1234")
     # ✅ Securely load database URL from environment variables
     SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://thomas:nFYYwWVe5DAYmKWyhsb9fufxmHn9ryOc@dpg-cvb8lnqn91rc739e3q00-a.oregon-postgres.render.com/tomchat_db_brky") + "?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
