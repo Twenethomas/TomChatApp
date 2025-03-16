7efe609e5e288a57d36ca8c45f28da4b921ad2b2
Twene Twene Appiah <159663841+Twenethomas@users.noreply.github.com>
2025-03-16 13:26:36 +0000

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -5,8 +5,4 @@ from flask_socketio import SocketIO
 # Initialize extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
-socketio = SocketIO(cors_allowed_origins="*")  
-@login_manager.user_loader
-def load_user(user_id):
-    from models import Users
-    return Users.query.get(user_id)
+socketio = SocketIO(cors_allowed_origins="*")
