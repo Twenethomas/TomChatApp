d8a6d8c4d259840dbb291d2846edf62f0fb41996
Twene Twene Appiah <159663841+Twenethomas@users.noreply.github.com>
2025-03-16 09:43:28 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "1234")
     # ✅ Securely load database URL from environment variables
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://thomas:nFYYwWVe5DAYmKWyhsb9fufxmHn9ryOc@dpg-cvb8lnqn91rc739e3q00-a.oregon-postgres.render.com/tomchat_db_brky") + "?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://thomas:nFYYwWVe5DAYmKWyhsb9fufxmHn9ryOc@dpg-cvb8lnqn91rc739e3q00-a.oregon-postgres.render.com/tomchat_db_brky") 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'txt'}
